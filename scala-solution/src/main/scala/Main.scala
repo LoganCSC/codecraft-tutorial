07a5e22c22a747cf27dcea4d0000aa1df9596053
barrybecker4 <barrybecker4@yahoo.com>
2017-10-28 07:38:59 -0700

    Remove my experiment1 package (moved to branch).

--- scala-solution/src/main/scala/Main.scala
+++ scala-solution/src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import cwinter.codecraft.core.api._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    TheGameMaster.runLevel2(new experiment1.Mothership)
+    TheGameMaster.runLevel2(new tutorial.Mothership)
   }
 }
 
