bbd14b2e2f745e1e0b317b71202d5e0ba09df54a
barrybecker4 <barrybecker4@yahoo.com>
2017-10-28 07:39:47 -0700

    Add comment about runLevel.

--- scala-solution/src/main/scala/Main.scala
+++ scala-solution/src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import cwinter.codecraft.core.api._
 
 object Main {
   def main(args: Array[String]): Unit = {
+    // Select runLevelN where N goes from 1 to 5 and gets progressively harder.
     TheGameMaster.runLevel2(new tutorial.Mothership)
   }
 }
